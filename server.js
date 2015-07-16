0af6e2a49bedf0567516cbd1b3b74fa0f1dfd603
Shakir Ali <shakirali2244@yahoo.com>
2015-07-15 23:58:44 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('apassara <3 node.js on Azure\n');
+  res.end('shakir <3 node.js on Azure\n');
 }).listen(port);
